335ebc8b5d58c1b053e508e981fc5ac6ab738c53
Wenpin Liao <wenpin.liao@tpisoftware.com>
2024-04-24 01:21:57 +0800

    first commit

---
+++ bin/master_flutter_30days_01.dart
@@ -0,0 +1,5 @@
+import 'package:master_flutter_30days_01/master_flutter_30days_01.dart' as master_flutter_30days_01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${master_flutter_30days_01.calculate()}!');
+}
---
+++ test/master_flutter_30days_01_test.dart
@@ -0,0 +1,8 @@
+import 'package:master_flutter_30days_01/master_flutter_30days_01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
